d59ed298bb4d5dca848426996506545d52f781bb
Alexander Kjäll <alexander.kjall@gmail.com>
2021-12-15 17:18:29 +0100

    day 6, part 2

--- src/day6.rs
+++ src/day6.rs
@@ -5,6 +5,7 @@ use nom::character::complete::digit1;
 use nom::combinator::{map_res, recognize};
 use nom::multi::many0;
 use nom::IResult;
+use std::collections::HashMap;
 use std::fs::read_to_string;
 
 pub fn calculate() -> Result<(String, String), Error> {
@@ -13,56 +14,65 @@ pub fn calculate() -> Result<(String, String), Error> {
     Ok((part1(&input)?, part2(&input)?))
 }
 
-fn my_u32(input: &str) -> IResult<&str, u32> {
+fn my_u64(input: &str) -> IResult<&str, u64> {
     let (rest, data) = map_res(recognize(digit1), str::parse)(input)?;
     let (rest, _) = alt((tag(","), tag("\n")))(rest)?;
 
     Ok((rest, data))
 }
 
-fn multi(i: &str) -> IResult<&str, Vec<u32>> {
-    many0(my_u32)(i)
+fn multi(i: &str) -> IResult<&str, Vec<u64>> {
+    many0(my_u64)(i)
 }
 
-fn parse_input(input: &str) -> Result<Vec<u32>, Error> {
+fn parse_input(input: &str) -> Result<Vec<u64>, Error> {
     let (_, data) = multi(input)?;
 
     Ok(data)
 }
 
-fn calc(init: u32) -> u32 {
-    let mut state = vec![init];
-
-    for _ in 1..=80 {
-        let mut to_expand = vec![];
-        for fish in &mut state {
-            if *fish == 0 {
-                *fish = 6;
-                to_expand.push(8);
-            } else {
-                *fish -= 1;
-            }
-        }
-        state.extend(to_expand);
+fn calc_rec(init: u64, day: u64, cache: &mut HashMap<(u64, u64), u64>) -> u64 {
+    if day == 0 {
+        return 1;
     }
 
-    state.len() as u32
+    if init == 0 {
+        if !cache.contains_key(&(6, day - 1)) {
+            let val = calc_rec(6, day - 1, cache);
+            cache.insert((6, day - 1), val);
+        }
+        if !cache.contains_key(&(8, day - 1)) {
+            let val = calc_rec(8, day - 1, cache);
+            cache.insert((8, day - 1), val);
+        }
+        return *cache.get(&(6, day - 1)).unwrap() + *cache.get(&(8, day - 1)).unwrap();
+    } else {
+        if !cache.contains_key(&(init - 1, day - 1)) {
+            let val = calc_rec(init - 1, day - 1, cache);
+            cache.insert((init - 1, day - 1), val);
+        }
+        return *cache.get(&(init - 1, day - 1)).unwrap();
+    }
 }
 
-fn precompute() -> [u32; 6] {
-    [calc(0), calc(1), calc(2), calc(3), calc(4), calc(5)]
-}
+fn part1(input: &[u64]) -> Result<String, Error> {
+    let mut cache: HashMap<(u64, u64), u64> = HashMap::new();
 
-fn part1(input: &[u32]) -> Result<String, Error> {
-    let lookup = precompute();
-
-    let result = input.iter().fold(0u32, |s, n| s + lookup[*n as usize]);
+    let result = input
+        .iter()
+        .fold(0u64, |s, n| s + calc_rec(*n, 80, &mut cache));
 
     Ok(format!("{}", result))
 }
 
-fn part2(_input: &[u32]) -> Result<String, Error> {
-    Ok(format!(""))
+fn part2(input: &[u64]) -> Result<String, Error> {
+    let mut cache: HashMap<(u64, u64), u64> = HashMap::new();
+
+    let result = input
+        .iter()
+        .fold(0u64, |s, n| s + calc_rec(*n, 256, &mut cache));
+
+    Ok(format!("{}", result))
 }
 
 #[test]
@@ -79,5 +89,5 @@ pub fn test_part1() {
 
 #[test]
 pub fn test_part2() {
-    assert_eq!("", part2(&vec![3, 4, 3, 1, 2]).unwrap());
+    assert_eq!("26984457539", part2(&vec![3, 4, 3, 1, 2]).unwrap());
 }
